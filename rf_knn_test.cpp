bba91c914c268377916569e3297e094c9b2b46a7
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-02-13 22:43:41 -0200

    recompiled test_lazy

--- rf_knn_test.cpp
+++ rf_knn_test.cpp
@@ -1,6 +1,5 @@
 // OpenCV
 #include <iostream>
-#include "Dataset.h"
 
 #include "tcpp/tree.hpp"
 
@@ -8,67 +7,17 @@
 #include "tcpp/rf.hpp"
 #include "tcpp/knn.hpp"
 
-
 using namespace std;
-
-
-void tccpPrepareTrainSamples(RF_KNN* rf, Dataset &training, unsigned int K)
-{
-	printf("Allocation Matrix %dx%d...\n", K , training.dimension());
-/*
-	vector<DTDocument> samples;
-
-	for(int i = 0; i < K; i++) {
-        DTDocument *doc = new DTDocument(); //samples.push_back(doc);
-
-        unsigned int idx = i;//sim.doc_id;
-		
-		doc->set_id(Utils::toString(idx));
-		doc->set_class(Utils::toString(training.getSamples()[idx].y));
-
-        std::map<unsigned int, double>::iterator it;
-		for(it = training.getSamples()[idx].features.begin(); it != training.getSamples()[idx].features.end(); ++it){
-			unsigned int term_id = it->first;
-			double term_count = it->second;
-			doc->insert_term(term_id, term_count);
-		
-			rf->insert_knn_term(term_id, doc, term_cout);
-		}
-
-		rf->add_document(doc);
-    }
-
-    //rf->updateIDF();
-  	//rf->updateTFIDF();*/
-
-    printf("finish...\n");
-
-}
-
-DTDocument * tcppPrepareSample(std::map<unsigned int, double> test_features, Dataset &training){
-	DTDocument * doc = new DTDocument();
-
-	std::map<unsigned int, double>::iterator it;
-	for(it = test_features.begin(); it != test_features.end(); ++it){
-		unsigned int term_id = it->first;
-		double term_count = it->second;
-
-		doc->insert_term(term_id, term_count);
-	}
-	return doc;
-}
 
 int main(int argc, char const *argv[])
 {
-	Dataset training, test_set;
-	/*
 	RF_KNN * knn_rf = new RF_KNN(0, 0.03, 30, 200);
 	  
 	knn_rf->train(argv[1]);
 
 	knn_rf->set_output_file(argv[3]);
 	knn_rf->test(argv[2]);
-	*/
+	
 
 	/*RF * rf = new RF(0, 0.03, 200);
 	  
@@ -78,12 +27,12 @@ int main(int argc, char const *argv[])
 	rf->test(argv[2]);
 	*/
 
-	knn * kNN = new knn();
+	/*knn * kNN = new knn();
 	  
 	kNN->train(argv[1]);
 
 	kNN->set_output_file(argv[3]);
-	kNN->test(argv[2]);
+	kNN->test(argv[2]);*/
 	
     
 	return EXIT_SUCCESS	;
