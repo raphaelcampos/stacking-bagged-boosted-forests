5871bd11ecdb8ed49d9fdeb88e2ac83341c72b28
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-03-22 09:08:59 -0300

    [FEATURE] calculate macro and micro f1 with scikit

---
+++ release/stats.py
@@ -0,0 +1,31 @@
+from sklearn.metrics import f1_score,classification_report
+
+import numpy as np
+
+import argparse
+
+folds_macro = []
+folds_micro = []
+
+parser = argparse.ArgumentParser(description="This script creates Venn's diagram.")
+
+parser.add_argument("result", type=str,
+                    help="result files. Each row needs to be in the following format:\n\t\"doc id\" \"actual class\" \"predicted class\"")
+args = parser.parse_args()
+
+arr = np.loadtxt(args.result)
+
+folds = np.split(arr, np.where(arr[:,0] == 0)[0])
+
+for fold in folds:
+	pred, y_test = fold[:,1], fold[:,2]
+	folds_micro = folds_micro + [f1_score(y_true=y_test, y_pred=pred, average='micro')]
+	folds_macro = folds_macro + [f1_score(y_true=y_test, y_pred=pred, average='macro')]
+
+
+print folds_micro, folds_macro
+folds_micro.pop(0)
+folds_macro.pop(0)
+print "F1-Score"
+print "\tMicro: ", np.mean(folds_micro)*100, np.std(folds_micro, ddof=1)*100
+print "\tMacro: ", np.average(folds_macro)*100, np.std(folds_macro,ddof=1)*100
