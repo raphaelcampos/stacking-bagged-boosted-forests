c25aaccda3d3c7cc52ceb72b8514e823ccc7e300
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-04-02 08:27:35 -0300

    [EXP] gridsearch for n datasets

--- release/exp_gridsearch.sh
+++ release/exp_gridsearch.sh
@@ -6,44 +6,44 @@ trials=5
 datasets=('20ng' 'reuters90' 'acm')
 
 ################################################################################
-#				 					4UNI 									   #
+#				 					${dataset} 									   #
 ################################################################################ 
 for dataset in ${datasets}
 do
 	#Suport Vector Machine (LIBSVM)
 	method=svm
-	python ../python/main.py -m ${method} --cv 5 -g 0 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	python ../python/main.py -m ${method} --cv 5 -g 0 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# kNearestNeighbors
 	method=knn
-	python ../python/main.py -m ${method} --cv 5 -g 0 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	python ../python/main.py -m ${method} --cv 5 -g 0 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# Naive Bayes
 	method=nb
-	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# Random Forest
 	method=rf
-	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# Extremely Randomized Trees
 	method=xt
-	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# Lazy Random Forest
 	#method=lazy
-	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# Lazy Extremely Randomized Trees
 	#method=lxt
-	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# BROOF
 	#method=broof
-	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 
 	# BERT
 	#method=bert
-	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_4uni ${dataset_dir}/webkb.svm > ${output_dir}/grid_${method}_4uni
+	#python ../python/main.py -m ${method} --cv 5 -g 1 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${method}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${method}_${dataset}
 done
 ################################################################################
