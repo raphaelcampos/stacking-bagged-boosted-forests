c44bde294ab0a62ceb3eb59e2d54d2d66274bfb8
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-04-24 11:00:04 -0300

    [EXP][FIX] correct use of array in bash script

--- release/exp_norm.sh
+++ release/exp_norm.sh
@@ -9,11 +9,11 @@ norms=('max' 'l1' 'l2')
 ################################################################################
 #				 					${dataset} 								#
 ################################################################################ 
-for dataset in ${datasets}
+for dataset in ${datasets[@]}
 do
 	#Suport Vector Machine (LIBSVM)
 	method=svm
-	for norm in ${norms}
+	for norm in ${norms[@]}
 	do
 		python ../python/main.py -m ${method} --cv 5 -n ${norm} -g 0 -j ${n_jobs} --trials ${trials} --o ${output_dir}/results_${norm}_${dataset} ${dataset_dir}/${dataset}.svm > ${output_dir}/grid_${norm}_${dataset}
 	done
