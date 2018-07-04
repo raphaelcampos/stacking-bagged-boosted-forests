74daf0047f256e32acfc5733afd33b287089e2a9
raphaelcampos <raphaelrcampos@yahoo.com.br>
2018-07-03 22:26:30 -0300

    modularizando

--- release/stats.py
+++ release/stats.py
@@ -4,6 +4,27 @@ import numpy as np
 
 import argparse
 
+def stats(results):
+  folds_macro = []
+  folds_micro = []
+
+  arr = np.loadtxt(results)
+
+  folds = np.split(arr, np.where(arr[:,0] == 0)[0])
+
+  for fold in folds:
+    pred, y_test = fold[:,1], fold[:,2]
+    folds_micro = folds_micro + [f1_score(y_true=y_test, y_pred=pred, average='micro')]
+    folds_macro = folds_macro + [f1_score(y_true=y_test, y_pred=pred, average='macro')]
+
+  folds_micro.pop(0)
+  folds_macro.pop(0) 
+
+  return np.mean(folds_micro)*100, \
+          np.average(folds_macro)*100
+
+
+
 folds_macro = []
 folds_micro = []
 
